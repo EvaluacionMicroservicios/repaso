24841896d3526d0918783526e09e132057524393
Ronnie Gonzales <ronnie_cepafs@hotmail.com>
2025-01-25 04:02:28 -0500

    Primer commit

---
+++ src/main/java/com/bcp/repaso/repasomicroservicio/RepasomicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.bcp.repaso.repasomicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasomicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasomicroservicioApplication.class, args);
+	}
+
+}
